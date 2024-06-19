a2ce8972552d4118048eb7ab549292a97e73ed69
Jillian Armonia <jillian.marice@gmail.com>
2024-06-19 14:21:47 +0900

    finished phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,17 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (typeof n === "number"){
+    if (n < 1 || n > 1000000){
+      throw new Error('Not in range. Choose from 1 to 1000000');
+    }
+    
+    return 1 / n;
+  } else {
+    throw new TypeError('Not a number');
+  }
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const { expect } = require('chai');
+
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe('returnsThree', function(){
+    it('should return the number 3', function(){
+        expect(returnsThree()).to.be.equal(3);
+    })
+})
+
+describe('reciprocal', function(){
+
+    context('when arguments are valid', function(){
+        it('should return the reciprocal of an argument', function(){
+            expect(reciprocal(10)).to.be.equal(1/10);
+            expect(reciprocal(5)).to.be.equal(1/5);
+        });
+    })
+
+    context('when arguments are invalid', function(){
+        it('should throw an error if n is not a number', function(){
+            expect(() => reciprocal('string')).to.throw(TypeError, 'number');
+        })
+
+        it('should throw an error if n < 1', function(){
+            expect(() => reciprocal(0)).to.throw(Error, 'range');
+        })
+
+        it('should throw an error if n > 1000000', function(){
+            expect(() => reciprocal(1000001)).to.throw(Error, 'range');
+        })
+    })
+})
