6484785c12c7cd484374a29df912d1839fec7a88
Jillian Armonia <jillian.marice@gmail.com>
2024-06-19 13:40:26 +0900

    finished phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,15 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  let reversed = "";
+
+  if (typeof string === "string"){
+    for (let i = string.length - 1; i >= 0; i--){
+      reversed += string[i];
+    };
+
+    return reversed;
+
+  } else {
+    throw new TypeError('Not a string');
+  }
+
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,16 @@
 // Your code here
+const { expect } = require('chai');
+
+const reverseString = require('../problems/reverse-string')
+
+describe('reverseString', function(){
+    let string = "fun";
+
+    it('should return a reversed string', function(){
+        expect(reverseString(string)).to.be.equal("nuf");
+    });
+
+    it('should throw an error if the argument is not a string', function(){
+        expect(reverseString).to.throw(TypeError);
+    })
+})
