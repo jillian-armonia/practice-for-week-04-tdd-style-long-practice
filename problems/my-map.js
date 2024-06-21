d9f87acf17a4d7b5f534606112ad22986fd61e13
Jillian Armonia <jillian.marice@gmail.com>
2024-06-21 13:56:12 +0900

    finished phase

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,11 @@
 function myMap(inputArray, callback) {
-  // Your code here
+    let mapped = [];
+
+    for (let i = 0; i < inputArray.length; i++){
+        mapped.push(callback(inputArray[i]));
+    }
+
+    return mapped;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const chai = require('chai');
+const { expect } = require('chai');
+const spies = require("chai-spies");
+chai.use(spies);
+
+const myMap  = require('../problems/my-map');
+
+describe('myMap', function(){
+    let array;
+    let callback;
+
+    beforeEach(function(){
+        array = [1, 2, 3];
+        callback = el => el * 2;
+    });
+
+    it('should not mutate the passed-in array argument', function(){
+        myMap(array, callback);
+        expect(array).to.eql([1, 2, 3]);
+    });
+
+    it('should not use the built-in method Array.map', function(){
+        const spy = chai.spy.on(array, 'map');
+        myMap(array, callback);
+        expect(spy).to.not.have.been.called();
+    });
+
+    it('should invoke passed-in callback function on each element of the passed-in array', function(){
+        const spy = chai.spy(callback);
+        myMap(array, spy);
+        expect(spy).to.have.been.called.exactly(array.length);
+    })
+})
